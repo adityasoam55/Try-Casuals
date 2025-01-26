0b4be62d9edd07a9a944d31d1e68aa9730fce6fc
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2025-01-26 14:35:54 +0530

    All the components moved inside components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import MainPage from './MainPage';
+import MainPage from './components/MainPage';
 import { Route, Routes } from 'react-router-dom';         
-import NavBar from './NavBar';
-import Footer from './Footer';
-import AllProducts from './AllProducts';
-import CategoryList from './CategoryList';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import AllProducts from './components/AllProducts';
+import CategoryList from './components/CategoryList';
 
 function App() {
   return (
