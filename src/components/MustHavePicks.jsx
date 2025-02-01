600eb20984603d007825d73a7984bf052a7377e9
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2025-02-01 15:41:12 +0530

    MustHavePicks component updated

--- src/components/MustHavePicks.jsx
+++ src/components/MustHavePicks.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getProductWomensDresses } from './api';
+import { Link } from 'react-router-dom';
 
 
 function MustHavePicks() {
@@ -30,19 +31,21 @@ function MustHavePicks() {
 
             <div className='flex overflow-x-scroll sans-serif pb-4 gap-2 px-10'>
                 {
-                    itemList.map(function (item) {
+                    itemList.map(function ({id, ...item}) {
                         return (
-                            <div key={item.id} className=' text-white bg-gray-400'>
-                                <div className='h-96 w-80'>
-                                    <img className='w-full h-full object-cover' src={item.thumbnail} />
+                            <Link to={"/productdetails/" + id}>
+                                <div key={item.id} className=' text-white bg-gray-400'>
+                                    <div className='h-96 w-80'>
+                                        <img className='w-full h-full object-cover' src={item.thumbnail} />
+                                    </div>
+                                    <div className='w-80 h-32 flex flex-col gap-1 px-2'>
+                                        <h3 className='text-xl font-thin'>{item.title}</h3>
+                                        <p className='text-sm font-thin'>{item.category}</p>
+                                        <p className='font-thin'>${item.price}</p>
+                                        <p className='text-sm'>ratings - {item.rating}/5</p>
+                                    </div>
                                 </div>
-                                <div className='w-80 h-32 flex flex-col gap-1 px-2'>
-                                    <h3 className='text-xl font-thin'>{item.title}</h3>
-                                    <p className='text-sm font-thin'>{item.category}</p>
-                                    <p className='font-thin'>${item.price}</p>
-                                    <p className='text-sm'>ratings - {item.rating}/5</p>
-                                </div>
-                            </div>
+                            </Link>
                         )
                     })
                 }
