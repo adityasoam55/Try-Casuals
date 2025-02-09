b87a0c4c8c8f0b19c72d9c8a030b181e677739cf
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2025-02-09 15:08:17 +0530

    mainimage changed

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -5,7 +5,7 @@ function MainImage() {
     <div className="w-full h-screen bg-white box-border">
       <div className="relative w-full h-full">
         <img className='w-full h-full object-cover'
-          src="https://images.unsplash.com/photo-1675218739622-8930d60cf7fd?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="/images/mainimage.avif"
           alt="Background"
         />
 
