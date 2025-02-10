16b7c0f5e022b87a5d1d16268ea4643193eea847
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2025-02-10 11:43:41 +0530

    NewArrivals image changed

--- src/components/NoticePage.jsx
+++ src/components/NoticePage.jsx
@@ -7,7 +7,7 @@ function NoticePage() {
         <div className='w-screen  grid  md:grid-cols-1 lg:grid-cols-3 max-md:gap-6 gap-9 max-md:px-5 px-10  max-md:pb-16 pb-28'>
             <Link to="/newarrivals/" >
                 <div className='h-96 relative'>
-                    <img className='h-full w-full object-cover ' src="https://images.unsplash.com/photo-1516404813354-243addcc4762?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM3fHxsYWR5JTIwZGFuY2luZyUyMG9uJTIwYmVkfGVufDB8fDB8fHww" />
+                    <img className='h-full w-full object-cover ' src="/images/newarrivals.jpg" />
                     <div className='flex justify-center items-center w-full h-full absolute top-0 z-10 text-white font-black text-3xl font-mono hover:underline underline-offset-8  hover:cursor-pointer'>
                         <a className='opacity-100 relative' href="">New Arrivals</a>
                     </div>
